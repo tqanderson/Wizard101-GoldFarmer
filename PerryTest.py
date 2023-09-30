68ddf6b59dd86a7a65af3e91cf39b3887140d60b
Thomas Anderson <89548943+tqanderson@users.noreply.github.com>
2023-09-30 16:36:53 +0000

    Update PerryTest.py

--- PerryTest.py
+++ PerryTest.py
@@ -1,2 +1,41 @@
-from wizwalker import *
+import asyncio
 
+from wizwalker import ClientHandler
+from wizwalker.combat import CombatHandler
+
+
+HITNAME = "tempest"
+
+
+class LostSoulDestroyer(CombatHandler):
+    async def handle_round(self):
+        try:
+            hit = await self.get_card_named(HITNAME)
+        except ValueError:
+            print(f"No cards named {HITNAME} in hand.")
+        else:
+            monsters = await self.get_all_monster_members()
+            lost_soul = monsters[0]
+
+            print(f"Casting {HITNAME} on lost soul")
+            await hit.cast(lost_soul)
+
+
+async def main():
+    handler = ClientHandler()
+    client = handler.get_new_clients()[0]
+
+    try:
+        print("Preparing")
+        await client.activate_hooks()
+        await client.mouse_handler.activate_mouseless()
+        print("Ready for battle")
+
+        await LostSoulDestroyer(client).wait_for_combat()
+    finally:
+        print("Closing")
+        await handler.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
